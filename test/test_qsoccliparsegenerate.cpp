a644effc348bdbf162169f2f2bf0d2acd645d813
Huang Rui <vowstar@gmail.com>
2025-03-26 07:49:38 +0800

    test: add test_qsoccliparsegenerate test suite
    - Add new test target for qsoccliparsegenerate in CMakeLists.txt
    - Implement test cases for generate command with various scenarios
    
    Signed-off-by: Huang Rui <vowstar@gmail.com>

---
+++ test/test_qsoccliparsegenerate.cpp
@@ -0,0 +1,92 @@
+#include "cli/qsoccliworker.h"
+#include "common/config.h"
+
+#include <QDir>
+#include <QFile>
+#include <QStringList>
+#include <QTextStream>
+#include <QtCore>
+#include <QtTest>
+
+struct TestApp
+{
+    static auto &instance()
+    {
+        static auto                  argc      = 1;
+        static char                  appName[] = "qsoc";
+        static std::array<char *, 1> argv      = {{appName}};
+        /* Use QCoreApplication for cli test */
+        static const QCoreApplication app = QCoreApplication(argc, argv.data());
+        return app;
+    }
+};
+
+class Test : public QObject
+{
+    Q_OBJECT
+
+private slots:
+    void initTestCase() { TestApp::instance(); }
+
+    void testGenerateCommandHelp()
+    {
+        QSocCliWorker     socCliWorker;
+        const QStringList appArguments = {"qsoc", "generate", "--help"};
+        socCliWorker.setup(appArguments, false);
+        socCliWorker.run();
+
+        /* Just verify the command doesn't crash */
+        QVERIFY(true);
+    }
+
+    void testGenerateVerilogHelp()
+    {
+        QSocCliWorker     socCliWorker;
+        const QStringList appArguments = {"qsoc", "generate", "verilog", "--help"};
+        socCliWorker.setup(appArguments, false);
+        socCliWorker.run();
+
+        /* Just verify the command doesn't crash */
+        QVERIFY(true);
+    }
+
+    void testGenerateWithInvalidOption()
+    {
+        QSocCliWorker     socCliWorker;
+        const QStringList appArguments = {"qsoc", "generate", "verilog", "--invalid-option"};
+        socCliWorker.setup(appArguments, false);
+        socCliWorker.run();
+
+        /* Just verify the command doesn't crash */
+        QVERIFY(true);
+    }
+
+    void testGenerateWithMissingRequiredArgument()
+    {
+        QSocCliWorker     socCliWorker;
+        const QStringList appArguments = {
+            "qsoc", "generate", "verilog"
+            /* Missing netlist file argument */
+        };
+        socCliWorker.setup(appArguments, false);
+        socCliWorker.run();
+
+        /* Just verify the command doesn't crash */
+        QVERIFY(true);
+    }
+
+    void testGenerateWithVerbosityLevels()
+    {
+        QSocCliWorker     socCliWorker;
+        const QStringList appArguments = {"qsoc", "--verbose=3", "generate", "verilog", "--help"};
+        socCliWorker.setup(appArguments, false);
+        socCliWorker.run();
+
+        /* Just verify the command doesn't crash */
+        QVERIFY(true);
+    }
+};
+
+QTEST_APPLESS_MAIN(Test)
+
+#include "test_qsoccliparsegenerate.moc"
